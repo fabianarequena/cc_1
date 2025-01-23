7e413aa3f2af37e824d1847bb79c0e9dcf44e88e
fabianarequena <fabianarequena@usf.edu>
2025-01-23 15:06:31 -0500

    Task 1 - Employee Information

--- cc_1.js
+++ cc_1.js
@@ -3,3 +3,5 @@ const employeeID = "123";
 var isActive = true 
 
 console.log("Employee Name:" employeeName, "Type:", typeof employeeName);
+console.log("Employee ID:" employeeID, "Type:", typeof employeeID);
+console.log("Is Active:" isActive, "Type:" typeof isActive);
