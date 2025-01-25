e71156894cb73406aa2c3433ed303440d452c654
fabianarequena <fabianarequena@usf.edu>
2025-01-25 14:09:44 -0500

    Task 2 - Product Details

--- cc_1.js
+++ cc_1.js
@@ -1,7 +1,17 @@
+//Task 1: Employee Information
 let employeeName = "Fabiana Requena";
-const employeeID = "123";
+const employeeID = 123;
 var isActive = true 
 
 console.log("Employee Name:" employeeName, "Type:", typeof employeeName);
 console.log("Employee ID:" employeeID, "Type:", typeof employeeID);
 console.log("Is Active:" isActive, "Type:" typeof isActive);
+
+//Task 2: Product Details
+let productName = "Laptop";
+const productPrice = 999.99;
+var isAvailable = true;
+
+console.log("Product Name:", productName, "Type:", typeof, productName);
+console.log("Product Price:", productPrice, "Type:", typeof productPrice);
+console.log("Is Available:", isAvailable, "Type:", typeof isAvailable);
